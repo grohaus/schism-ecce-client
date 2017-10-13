3dd14cdb03da14bd5e6e14863f1a2cfa39f1dd5a
dhuddell <dan.huddell@gmail.com>
2017-10-13 12:08:33 -0400

    resources and data

--- __mocks__/fileMock.js
+++ __mocks__/fileMock.js
@@ -1,22 +1,25 @@
 module.exports = {
-  contacts: [{
-    name: "Jim",
-    photo: ":grinning:",
-    id: "1"
-  },
-  {
-    name: "Jane",
-    photo: ":upside_down:",
-    id: "2"
-  },
-  {
-    name: "Joe",
-    photo: ":alien:",
-    id: "3"
-  },
-  {
-    name: "Jiminy",
-    photo: ":robot:",
-    id: "4"
-  }]
+  contacts:
+  [
+    {
+      name: "Jim",
+      photo: "https://cdn.shopify.com/s/files/1/1061/1924/products/Smiling_Face_with_Halo_large.png?v=1480481056",
+      id: "1"
+    },
+    {
+      name: "Jane",
+      photo: "https://cdn.shopify.com/s/files/1/1061/1924/products/Ok_Hand_Sign_Emoji_large.png?v=1480481046",
+      id: "2"
+    },
+    {
+      name: "Joe",
+      photo: "https://cdn.shopify.com/s/files/1/1061/1924/products/Grinmacing_Face_Emoji_large.png?v=1480481035",
+      id: "3"
+    },
+    {
+      name: "Jiminy",
+      photo: "https://cdn.shopify.com/s/files/1/1061/1924/products/Tears_of_Joy_Emoji_8afc0e22-e3d4-4b07-be7f-77296331c687_large.png?v=1480481057",
+      id: "4"
+    }
+  ]
 };
